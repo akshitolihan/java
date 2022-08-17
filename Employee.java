bea21b0dd0349fd944c651de8462e9152e4788a2
akshitvilager <kumakshit15@gmail.com>
2022-08-17 14:52:41 +0530

    17aug

--- Employee.java
+++ Employee.java
@@ -1,83 +1,100 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Employee {
-    static String[] empHead = { "EmpNo", "EmpName", "JoinDate", "Designation Code", "Department", "Basic", "HRA",
-            "IT" };
-    static String[][] empDetail = new String[][] {
-            { "1001", "Ashish", "01/04/2009", "e", "R&D", "20000", "8000", "3000" },
-            { "1002", "Sushma", "23/08/2012", "c", "PM", "30000", "12000", "9000" },
-            { "1003", "Rahul", "12/11/2008", "k", "Acct", "10000", "8000", "1000" },
-            { "1004", "Chahat", "29/01/2013", "r", "FrontDesk", "12000", "6000", "2000" },
-            { "1005", "Ranjan", "16/07/2005", "m", "Engg", "50000", "20000", "20000" },
-            { "1006", "Suman", "1/1/2000", "e", "Manufacturing", "23000", "9000", "4400" },
-            { "1007", "Tanmay", "12/06/2006", "c", "PM", "29000", "12000", "10000" }
-    };
+    String empId;
+    String depName;
+    String empDesignation;
+    String empName;
+    String dateJoin;
+    int basic;
+    int hra;
+    int it;
+    char designationCode;
+    public static int da;
 
-    static String[] desgHead = { "Designation Code", "Designation", "DA" };
-    static String[][] desgDetail = new String[][] {
-            { "e", "Engineer", "20000" },
-            { "c", "Consultant", "32000" },
-            { "k", "Clerk", "12000" },
-            { "r", "Receptionist", "15000" },
-            { "m", "Manager", "40000" }
-    };
-
-    public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        int id = obj.nextInt();
-
-        switch (id) {
-            case 1001:
-                for (int i = 0; i < empHead.length; i++) {
-                    if (empHead[i] == "EmpNo" || empHead[i] == "EmpName" || empHead[i] == "Department") {
-                        System.out.print(empHead[i] + "\t");
-                    } else
-                        continue;
-                }
-                for (int i = 0; i < desgHead.length; i++) {
-                    if (desgHead[i] == "Designation") {
-                        System.out.print(desgHead[i] + "\t");
-                        System.out.print("Salary" + "\t");
-                    } else
-                        continue;
-                }
-                System.out.print("\n");
-
-                for (int i = 0; i <= empDetail.length; i++) {
-                    if (empHead[i] == "EmpNo" || empHead[i] == "EmpName" || empHead[i] == "Department") {
-                        System.out.print(empDetail[0][i] + "\t");
-                    }
-
-                    else
-                        continue;
-                }
-                for (int i = 0; i < desgHead.length; i++) {
-                    if (desgHead[i] == "Designation") {
-                        System.out.print(desgDetail[0][i] + "\t");
-                    }
-                    if (desgHead[i] == "Salary") {
-                        int salary;
-                        System.out.print(desgDetail[0][i] + "\t");
-                    } else
-                        continue;
-                }
-                break;
-
-            default:
-                break;
-        }
-        // int i=0;
-        // System.out.print(desgDetail[0][2] + empDetail[0][5] +"\t");
+    public Employee(String empId, String depName, String empDesignation, String empName, String dateJoin, int basic,
+            int hra, int it, char designationCode) {
+        this.empId = empId;
+        this.depName = depName;
+        this.empDesignation = empDesignation;
+        this.empName = empName;
+        this.dateJoin = dateJoin;
+        this.basic = basic;
+        this.hra = hra;
+        this.it = it;
+        this.designationCode = designationCode;
     }
 
-    // public static String getSalary() {
-    //     int i = 0;
-    //     String Salary[] = "0";
-    //     for (i = 0; i < empDetail.length; i++) {
-    //         Salary[i] = empDetail[i][i] ;
-    //     }
-    //     return Salary[];
-    // }
+    public static int da(char designationCode)
+
+    {
+        switch (designationCode) {
+            case 'e': {
+                da = 20000;
+                break;
+            }
+            case 'c': {
+                da = 32000;
+                break;
+            }
+            case 'k': {
+                da = 12000;
+                break;
+            }
+            case 'r': {
+                da = 15000;
+                break;
+            }
+            case 'm': {
+                da = 40000;
+                break;
+            }
+            default:
+                throw new IllegalStateException("Unexpected value: " + designationCode);
+        }
+        return da;
+    }
+
+    public static int salary(int basic, int hra, int da, int it) {
+        int salary = basic + hra + da - it;
+        return salary;
+    }
+
+    public static void details(String empId, String empName, String depName, String empDesignation, int salary) {
+        System.out.println("Emp Id\t\tEmployee Name\tDepartment\t\tDesignation\t\tSalary");
+        System.out
+                .println(empId + "\t\t" + empName + "\t\t" + depName + "\t\t\t" + empDesignation + "\t\t" + salary);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String empId;
+        int c = 0;
+        Employee[] employees = new Employee[3];
+        employees[0] = new Employee("1001", "R&D", "Engineer", "Ashish", "1/04/2009", 20000, 8000, 3000,
+                'e');
+        employees[1] = new Employee("1002", "PM", "Consultant", "Anjali", "23/08/2012", 30000, 12000, 9000, 'c');
+        employees[2] = new Employee("1003", "Acct", "Clerk", "Raju", "12/11/2008", 10000, 8000, 1000,
+                'k');
+        System.out.println("Enter the employee ID ");
+        empId = bufferedReader.readLine();
+        for (int i = 0; i < 3; i++) {
+            if (employees[i].empId.equals(empId)) {
+                c = 1;
+                int salary = salary(employees[i].basic, employees[i].hra, da(employees[i].designationCode),
+                        employees[i].designationCode);
+                details(employees[i].empId, employees[i].empName, employees[i].depName, employees[i].empDesignation,
+                        salary);
+                break;
+
+            }
+        }
+        if (c != 1)
+            System.out.println("Entered employee ID not found");
+    }
 
 }
+
--- Main.java
+++ Main.java
@@ -84,4 +84,125 @@ public class Main {
 
         return da;
     }
-}
+}// import java.util.Scanner;
+// import java.util.Arrays;
+
+// public class Employee {
+// static String[] empHead = { "EmpNo", "EmpName", "JoinDate", "Designation
+// Code", "Department", "Basic", "HRA",
+// "IT" };
+// static String[][] empDetail = new String[][] {
+// { "1001", "Ashish", "01/04/2009", "e", "R&D", "20000", "8000", "3000" },
+// { "1002", "Sushma", "23/08/2012", "c", "PM", "30000", "12000", "9000" },
+// { "1003", "Rahul", "12/11/2008", "k", "Acct", "10000", "8000", "1000" },
+// { "1004", "Chahat", "29/01/2013", "r", "FrontDesk", "12000", "6000", "2000"
+// },
+// { "1005", "Ranjan", "16/07/2005", "m", "Engg", "50000", "20000", "20000" },
+// { "1006", "Suman", "1/1/2000", "e", "Manufacturing", "23000", "9000", "4400"
+// },
+// { "1007", "Tanmay", "12/06/2006", "c", "PM", "29000", "12000", "10000" }
+// };
+
+// static String[] desgHead = { "Designation Code", "Designation", "DA" };
+// static String[][] desgDetail = new String[][] {
+// { "e", "Engineer", "20000" },
+// { "c", "Consultant", "32000" },
+// { "k", "Clerk", "12000" },
+// { "r", "Receptionist", "15000" },
+// { "m", "Manager", "40000" }
+// };
+
+// public static void main(String[] args) {
+
+// int[] basic = new int[empDetail.length];
+// for (int i = 0; i < empDetail.length; i++) {
+// basic[i] = Integer.parseInt(empDetail[i][5]);
+// }
+// for (int i = 0; i < basic.length; i++) {
+// System.out.println(basic[i]);
+// }
+
+// int[] hra = new int[empDetail.length];
+// for (int i = 0; i < empDetail.length; i++) {
+// hra[i] = Integer.parseInt(empDetail[i][5]);
+// }
+// for (int i = 0; i < hra.length; i++) {
+// System.out.println(hra[i]);
+// }
+// int[] it = new int[empDetail.length];
+// for (int i = 0; i < empDetail.length; i++) {
+// it[i] = Integer.parseInt(empDetail[i][5]);
+// }
+// for (int i = 0; i < it.length; i++) {
+// System.out.println(it[i]);
+// }
+// int[] da = new int[empDetail.length];
+// for (int i = 0; i < empDetail.length; i++) {
+// da[i] = Integer.parseInt(empDetail[i][5]);
+// }
+// for (int i = 0; i < da.length; i++) {
+// System.out.println(da[i]);
+// }
+
+// System.out.println("Akshit");
+// int[] salary = { 0, 5 };
+// for (int i = 0; i < empDetail.length; i++) {
+// salary[i] = basic[i] + hra[i] - it[i];
+// System.out.println(salary[i]);
+// }
+
+// Scanner obj = new Scanner(System.in);
+// int id = obj.nextInt();
+
+// switch (id) {
+// case 1001:
+// for (int i = 0; i < empHead.length; i++) {
+// if (empHead[i] == "EmpNo" || empHead[i] == "EmpName" || empHead[i] ==
+// "Department") {
+// System.out.print(empHead[i] + "\t");
+// } else
+// continue;
+// }
+// for (int i = 0; i < desgHead.length; i++) {
+// if (desgHead[i] == "Designation") {
+// System.out.print(desgHead[i] + "\t");
+// System.out.print("Salary" + "\t");
+// } else
+// continue;
+// }
+// System.out.print("\n");
+
+// for (int i = 0; i <= empDetail.length; i++) {
+// if (empHead[i] == "EmpNo" || empHead[i] == "EmpName" || empHead[i] ==
+// "Department") {
+// System.out.print(empDetail[0][i] + "\t");
+// }
+
+// else
+// continue;
+// }
+// for (int i = 0; i < desgHead.length; i++) {
+// if (desgHead[i] == "Designation") {
+// System.out.print(desgDetail[0][i] + "\t");
+// } else
+// continue;
+// }
+// break;
+
+// default:
+// break;
+// }
+// // int i=0;
+// // System.out.print(desgDetail[0][2] + empDetail[0][5] +"\t");
+// }
+
+// // public static String getSalary() {
+// // int i = 0;
+// // String Salary[] = "0";
+// // for (i = 0; i < empDetail.length; i++) {
+// // Salary[i] = empDetail[i][i] ;
+// // }
+// // return Salary[];
+// // }
+
+// }
